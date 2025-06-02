b754458611059ba5575f8e9702e3b80529e4a038
CamMouren <16133026+CamMouren@users.noreply.github.com>
2025-06-02 23:07:25 +0200

    Update movement.py

--- Script/movement.py
+++ Script/movement.py
@@ -7,32 +7,35 @@ class Movement:
     self.SPEED_y=SPEED_y
     self.POS_j_x=POS_j_x
     self.POS_j_y=POS_j_y
+    self.DIRECTIOn_S_x=DIRECTIOn_S_x
+    self.DIRECTIOn_S_y=DIRECTIOn_S_y
 
-def key_up(self, SPEED_y, POS_j_y, DIRECTION_S_x, DIRECTION_S_y):
+  
+  def key_up(self, SPEED_y, POS_j_y, DIRECTION_S_x, DIRECTION_S_y):
     if POS_j_y>18:
       if pyxel.btn(pyxel.KEY_UP):
         SPEED_y=-4
         POS_j_y+=SPEED_y
         DIRECTION_S_y=-25
         DIRECCTION_S_x=0
-
-def key_down((self, SPEED_y, POS_j_y, DIRECTION_S_x, DIRECTION_S_y):
+        
+  def key_down((self, SPEED_y, POS_j_y, DIRECTION_S_x, DIRECTION_S_y):
     if POS_j_y<734:
       if pyxel.btn(pyxel.KEY_DOWN):
         SPEED_y=4
         POS_j_y+=SPEED_y
         DIRECTION_S_y=30
         DIRECCTION_S_x=0
-
-def key_left((self, SPEED_x, POS_j_x, DIRECTION_S_x, DIRECTION_S_y):
+        
+  def key_left((self, SPEED_x, POS_j_x, DIRECTION_S_x, DIRECTION_S_y):
     if POS_j_y<734:
       if pyxel.btn(pyxel.KEY_LEFT):
         SPEED_x=4
         POS_j_x+=SPEED_x
         DIRECTION_S_y=0
         DIRECCTION_S_x=-25
-
-def key_left((self, SPEED_x, POS_j_x, DIRECTION_S_x, DIRECTION_S_y):
+        
+  def key_left((self, SPEED_x, POS_j_x, DIRECTION_S_x, DIRECTION_S_y):
     if POS_j_y>18:
       if pyxel.btn(pyxel.KEY_LEFT):
         SPEED_x=-4
